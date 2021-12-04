5f48900e4acc9d04e7fe61b8514538d9f8e7bb0c
Tobias Helmrich <tobias.g.helmrich@gmail.com>
2021-12-04 16:25:04 +0100

    chore: Start implementing example for adding addons

---
+++ examples/storybook/5-addons/src/components/GlobalStyle/GlobalStyle.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Inter', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
